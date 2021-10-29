be4b607dedce6e607af3a2386930fafe470479c2
harimrim <kigkfla@naver.com>
2021-10-29 19:21:22 +0900

    10/29 1시간테스트 ㅠㅠ

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from datetime import datetime
-
+from datetime import datetime, timedelta
+import jwt
+import hashlib
+import os
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+
 
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.dbStock
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
@@ -38,15 +43,37 @@ def save_post():
 @app.route('/articles', methods=['GET'])
 def get_posts():
     order = request.args.get('order')
+    per_page = request.args.get('perPage')
+    cur_page = request.args.get('curPage')
+    search_title = request.args.get('searchTitle')
+    search_condition = {}
+    if search_title is not None:
+        search_condition = {"title": {"$regex": search_title}}
+
+    limit = int(per_page)
+    skip = limit * (int(cur_page) - 1)
+    total_count = db.article.find(search_condition).count()
+    total_page = int(total_count / limit) + (1 if total_count % limit > 0 else 0)
+
     if order == "desc":
-        articles = list(db.article.find({}, {'_id': False}).sort([("read_count", -1)]))
+        articles = list(db.article.find(search_condition, {'_id': False})
+                        .sort([("read_count", -1)]).skip(skip).limit(limit))
     else:
-        articles = list(db.article.find({}, {'_id': False}).sort([("reg_date", -1)]))
+        articles = list(db.article.find(search_condition, {'_id': False})
+                        .sort([("reg_date", -1)]).skip(skip).limit(limit))
 
     for a in articles:
         a['reg_date'] = a['reg_date'].strftime('%Y.%m.%d %H:%M:%S')
 
-    return jsonify({"articles": articles})
+    paging_info = {
+        "totalCount": total_count,
+        "totalPage": total_page,
+        "perPage": per_page,
+        "curPage": cur_page,
+        "searchTitle": search_title
+    }
+
+    return jsonify({"articles": articles, "pagingInfo": paging_info})
 
 
 @app.route('/article', methods=['DELETE'])
@@ -78,6 +105,38 @@ def update_read_count(idx):
     article = db.article.find_one({'idx': int(idx)}, {'_id': False})
     return jsonify({"article": article})
 
+@app.route('/login/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    userid_receive = request.form['userid_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = client.dbStock.find_one({'userid': userid_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': userid_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/login/sign_up', methods=['POST'])
+def sign_up():
+    userid_receive = request.form['userid_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "userid": userid_receive,
+        "password": password_hash
+    }
+    client.dbStock.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
